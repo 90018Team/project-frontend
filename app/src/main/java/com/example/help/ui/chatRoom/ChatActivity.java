6012b3a53e2716b52183a89340064cd4ffe2f018
kaile wang <s3543953@student.rmit.edu.au>
2021-10-10 14:37:21 +1100

    Basic settings, chatroom, and auth

--- app/src/main/java/com/example/help/ui/chatRoom/ChatActivity.java
+++ app/src/main/java/com/example/help/ui/chatRoom/ChatActivity.java
@@ -1,16 +1,174 @@
 package com.example.help.ui.chatRoom;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.help.R;
+import com.example.help.databinding.ActivityMainBinding;
+import com.example.help.models.Message;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.api.Distribution;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ChatActivity extends AppCompatActivity {
-
+    private static final String TAG = "ChatActivity";
+    private ActivityMainBinding binding;
+    private EditText editText;
+    private Button button;
+    private RecyclerView recyclerView;
+    private LinearLayoutManager linearLayoutManager;
+    private FirebaseRecyclerAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        // The FirebaseRecyclerAdapter class and options come from the FirebaseUI library
+        // See: https://github.com/firebase/FirebaseUI-Android
+        editText = findViewById(R.id.chat_edit);
+        button = findViewById(R.id.send);
+        recyclerView = findViewById(R.id.messages_list);
+
+        // button actions
+        button.setOnClickListener(view -> {
+            DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("posts").push();
+            Message m = new Message(editText.getText().toString(),getUserName(),getPhotoUrl(),null);
+            databaseReference.child("Message").push().setValue(m);
+            // clear the text
+            editText.setText("");
+        });
+        linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setHasFixedSize(true); // may not needed.
+
+        // fetch data
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference messageRef = database.getReference("message");
+        FirebaseRecyclerOptions<Message> options =
+                new FirebaseRecyclerOptions.Builder<Message>()
+                        .setQuery(messageRef, Message.class)
+                        .setLifecycleOwner(this)
+                        .build();
+        adapter = new FirebaseRecyclerAdapter<Message,MessageHolder>(options) {
+            @NonNull
+            @Override
+            public MessageHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.list_item, parent, false);
+                return new MessageHolder(view);
+            }
+
+            @Override
+            protected void onBindViewHolder(@NonNull MessageHolder holder, int position, @NonNull Message model) {
+                holder.setName(model.getName());
+                holder.setTxtDesc(model.getText());
+                int finalPosition = holder.getAdapterPosition();
+                holder.root.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(ChatActivity.this, String.valueOf(finalPosition), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+        };
+        recyclerView.setAdapter(adapter);
+//        binding = ActivityMainBinding.inflate(getLayoutInflater());
+//        binding.progressBar.visibility = ProgressBar.INVISIBLE;
+//        LinearLayoutManager manager = new LinearLayoutManager(this);
+//        manager.setStackFromEnd(true);
+//        binding.messageRecyclerView.layoutManager = manager;
+//        binding.messageRecyclerView.adapter = adapter;
+
+
+    }
+
+//    // deprecated method
+//    public void fetchData() {
+//        // Write a message to the database
+//        FirebaseDatabase database = FirebaseDatabase.getInstance();
+//        DatabaseReference messageRef = database.getReference("message");
+//        // Read from the database
+//        messageRef.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                // This method is called once with the initial value and again
+//                // whenever data at this location is updated.
+//                String value = dataSnapshot.getValue(String.class);
+//                Log.d(TAG, "Value is: " + value);
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError error) {
+//                // Failed to read value
+//                Log.w(TAG, "Failed to read value.", error.toException());
+//            }
+//        });
+//
+//    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+
+    private String getUserName(){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        return user.getDisplayName()==null?"ANONYMOUS":user.getDisplayName();
+    }
+    private String getPhotoUrl(){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        return user.getPhotoUrl()==null?"":user.getPhotoUrl().toString();
+    }
+
+    public class MessageHolder extends RecyclerView.ViewHolder {
+        public LinearLayout root;
+        public TextView txtTitle;
+        public TextView txtDesc;
+
+        public MessageHolder(View itemView) {
+            super(itemView);
+            root = itemView.findViewById(R.id.list_root);
+            txtTitle = itemView.findViewById(R.id.list_title);
+            txtDesc = itemView.findViewById(R.id.list_desc);
+        }
+
+        public void setName(String string) {
+            txtTitle.setText(string);
+        }
+
+
+        public void setTxtDesc(String string) {
+            txtDesc.setText(string);
+        }
     }
 }
