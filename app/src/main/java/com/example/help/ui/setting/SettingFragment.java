40e31f9c65b50aad4c321089c6614875378d3258
price-dj <dprice2@student.unimelb.edu.au>
2021-11-02 19:14:00 +1100

    more work on settings, done layout, now logic to be done

--- app/src/main/java/com/example/help/ui/setting/SettingFragment.java
+++ app/src/main/java/com/example/help/ui/setting/SettingFragment.java
@@ -1,17 +1,17 @@
 package com.example.help.ui.setting;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
-import androidx.preference.EditTextPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
 
 import com.example.help.MainActivity;
 import com.example.help.R;
-import com.example.help.ui.signIn.SignInActivity;
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -19,6 +19,9 @@ import com.google.android.gms.tasks.Task;
 
 public class SettingFragment extends PreferenceFragmentCompat {
     private final String TAG = "Setting fragment";
+    SharedPreferences sharedPrefs;
+    private Preference messageToggle;
+    //private Preference geolocationToggle;
     
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
@@ -28,7 +31,15 @@ public class SettingFragment extends PreferenceFragmentCompat {
             signOut();
             return true;
         });
-        EditTextPreference defaultMessagePreference = findPreference("default message");
+
+        messageToggle = findPreference("message");
+
+
+
+        // get shared preferences
+        sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+
+        //EditTextPreference defaultMessagePreference = findPreference("default message");
         // TODO: do something with default message preference
     }
     private void signOut() {
@@ -41,4 +52,7 @@ public class SettingFragment extends PreferenceFragmentCompat {
         });
 
     }
+
+
+
 }
